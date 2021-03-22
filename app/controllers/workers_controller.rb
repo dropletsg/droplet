631dc7d4ac212da0a9fa51f0e6eaea0c5482be45
jasonleewkd <jasonleewkd@gmail.com>
2021-03-23 00:24:32 +0800

    edited some more items in workers controller

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -14,23 +14,23 @@ class WorkersController < ApplicationController
   def create
     @worker = Worker.new(worker_params)
     @worker.save
-    # how to redirect to home page?
+    redirect_to workers_path
   end
 
-  # When updating an existing worker, the worker gets duplicated in the index page so there is now the original plus updated worker
   def edit
     @worker = Worker.find(params[:id])
   end
 
   def update
-    @worker = Worker.new(worker_params)
-    @worker.save
-    # how to redirect to home page?
+    @worker = Worker.find(params[:id])
+    @worker.update(worker_params)
+    redirect_to workers_path
   end
 
   private
 
+  # params use set?
   def worker_params
-    params.require(:worker).permit(:email, :name, :alias, :mobile_number, :description, :photo_id_front, :photo_id_back, :id_selfie, :id_type, :id_valid, :payment_link, :payment_qr, :is_archived)
+    params.require(:worker).permit(:email, :name, :alias, :mobile_number, :description, :id_type, :id_valid, :payment_link, :is_archived)
   end
 end
