ce5c60a16ab9b1a9102673aa64129a34b0a917ab
YuanMing Lee <yuanming.lee91@gmail.com>
2021-03-23 23:00:10 +0800

    Set up cloudinary and Active Storage

--- app/models/case.rb
+++ app/models/case.rb
@@ -4,4 +4,6 @@ class Case < ApplicationRecord
   belongs_to :user
   has_many :payments
   has_many :case_notes
+  has_one_attached :paid_proof
+  has_many_attached :files
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,4 +1,8 @@
 class Worker < ApplicationRecord
   has_many :cases
   has_many :case_notes
+  has_one_attached :photo_id_front
+  has_one_attached :photo_id_back
+  has_one_attached :id_selfie
+  has_one_attached :payment_qr
 end
