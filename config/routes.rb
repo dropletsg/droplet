196a7fb22aa287854447e44127475f244ce020f2
jiayen lee <jiiayenn.lee@gmail.com>
2021-03-24 22:03:47 +0800

    done for coordinator

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   
   resources :coordinators
-  
-
-=======
-
   resources :workers
->>>>>>> 4e20116c7c229ef722b42b31929bf343390ce372
 end
