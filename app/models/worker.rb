cc066ab86ce2f15d616580b00710af3b9186953c
jasonleewkd <jasonleewkd@gmail.com>
2021-03-27 16:21:51 +0800

    notes for workers and cases

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,6 +1,6 @@
 class Worker < ApplicationRecord
   has_many :cases
-  has_many :case_notes
+  has_many :worker_notes
   has_one_attached :photo_id_front
   has_one_attached :photo_id_back
   has_one_attached :id_selfie
