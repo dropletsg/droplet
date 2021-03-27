457c9df51674a91e2a842f053a1a98ae4b89bf7c
jasonleewkd <jasonleewkd@gmail.com>
2021-03-27 11:02:21 +0800

    workers update

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -5,5 +5,5 @@ class Worker < ApplicationRecord
   has_one_attached :photo_id_back
   has_one_attached :id_selfie
   has_one_attached :payment_qr
-  validates :email, :name
+  validates :email, :name, :alias, :mobile_number, :description, :photo_id_front, :photo_id_back, :id_selfie, :id_type, :id_valid, :payment_link, :payment_qr, :is_archived, presence: true
 end
