96911efac0dd9ddf087a0f7595482157ac472338
JaelynSng <jaelyn.sng@gmail.com>
2021-03-28 12:31:31 +0800

    passing hidden field in params to redirect back to case show page from worker controller Update action

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -22,18 +22,27 @@ class WorkersController < ApplicationController
   end
 
   def update
-    @worker.update(worker_params)
-    redirect_to worker_path
+    if upload_verification_doc?
+      case_id = Case.find(params[:worker][:case_id])
+      redirect_to case_path(case_id)
+    else
+      @worker.update(worker_params)
+      redirect_to worker_path
+    end
   end
 
   private
 
   # params use set?
   def worker_params
-    params.require(:worker).permit(:email, :name, :alias, :mobile_number, :description, :id_type, :id_valid, :payment_link, :payment_qr, :is_archived)
+    params.require(:worker).permit(:email, :name, :alias, :mobile_number, :description, :photo_id_front, :photo_id_back, :id_selfie, :id_type, :id_valid, :payment_link, :payment_qr, :is_archived)
   end
 
   def set_worker
     @worker = Worker.find(params[:id])
+  end
+
+  def upload_verification_doc?
+    params[:commit] == "Save"
   end
 end
