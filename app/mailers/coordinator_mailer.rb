732d9527fef6013a6d295715d6c669bddf33e3c5
YuanMing Lee <yuanming.lee91@gmail.com>
2021-04-17 16:29:09 +0800

    Update progress bar color

--- app/mailers/coordinator_mailer.rb
+++ app/mailers/coordinator_mailer.rb
@@ -1,9 +1,8 @@
 class CoordinatorMailer < ApplicationMailer
-    default from: 'testdropletproject@gmail.com'
+  default from: 'testdropletproject@gmail.com'
 
-    def notification_email
-        @active_case = params[:active_case]
-        mail(to: @active_case.coordinator.email, subject: "Case #{@active_case.id} is off-track")
-    end
-    
+  def notification_email
+    @active_case = params[:active_case]
+    mail(to: @active_case.coordinator.email, subject: "Case #{@active_case.id} is off-track")
+  end
 end
